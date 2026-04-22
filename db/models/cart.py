873bb41c07867dbd7fe18a51438a863a406e55fa
Craig Abernethy <craigabernethy85@gmail.com>
2026-04-21 21:44:41 -0700

    FreshBase Semantic Resolve: Intent 2

--- db/models/cart.py
+++ db/models/cart.py
@@ -4,3 +4,10 @@ class ShoppingCart:
 
     def add_item(self, name, price):
         self.items[name] = price
+    
+    def apply_discount(self, percentage):
+        for item in self.items:
+            original_price = self.items[item]
+            discount_amount = original_price * (percentage / 100)
+            new_price = original_price - discount_amount
+            self.items[item] = new_price
